a15aa4f02424ca7b04c20fc9f64bc927366780df
dmburik <dburik@mail.ru>
2025-01-10 00:15:43 +0300

    Add files via upload

---
+++ date_changer.py
@@ -0,0 +1,7 @@
+import greetings
+from greetings import created_date
+from greetings import issue_date
+temp_created_date=created_date[0:5]
+temp_issue_date=issue_date[0:5]
+print(created_date[0:5])
+print(issue_date[0:5])
