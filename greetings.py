1cfeb1bffb91e1222ca199fe240818c6eda273d2
dmburik <dburik@mail.ru>
2025-01-09 23:56:27 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+
+username="Dmitriy"
+title="no idea"
+content="no idea"
+status="just start"
+created_date="09.01.2025"
+issue_date="09.02.2025"
+print ("имя пользователя: "+username)
+print("имя заметки: "+title)
+print("краткое описание заметки: "+status)
+print ("дата создания: "+created_date)
+print("дата планируемого окончания: "+issue_date)
+
