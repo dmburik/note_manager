b9b03892b0da73e16ea35c0ce43c71350dfd8622
dmburik <dburik@mail.ru>
2025-01-21 18:58:46 +0300

    Add files via upload

--- final.py
+++ final.py
@@ -11,4 +11,11 @@ title_title = [input("введите название заголовка1"), inp
 temp_created_date=created_date[0:5]
 temp_issue_date=issue_date[0:5]
 note=[username,title,content,status,temp_created_date,temp_issue_date,title_title]
+print("Имя: "+note[0])
+print("Название заметки: "+note[1])
+print("Описание: "+note[2])
+print("Статус заметки: "+note[3])
+print("Дата создания: "+note[4])
+print("Дата окончания: "+note[5])
+print("Заголовки: "+title_title[0]+", "+title_title[1]+", "+title_title[2])
 print(note)
