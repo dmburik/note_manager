124a3b5e378776e3f77543a725117ed0d1b5d2c5
dmburik <dburik@mail.ru>
2025-01-23 22:00:47 +0300

    Add files via upload

--- check_deadline.py
+++ check_deadline.py
@@ -9,17 +9,17 @@ check_issue_date = input("введите дату дэдлайна в форма
 # конвертация полученной даты в формат date и запрос даты в случае не правильной даты
 def check_date():
     check_issue_date2=check_issue_date
-    issue_date=0
-    while issue_date==0:
+    local_issue_date=0
+    while local_issue_date==0:
         try:
-            issue_date=date(int(check_issue_date2[6:11]),int(check_issue_date2[3:5]),int(check_issue_date2[0:2]))
+            local_issue_date=date(int(check_issue_date2[6:11]),int(check_issue_date2[3:5]),int(check_issue_date2[0:2]))
         except ValueError:
             try:
-                issue_date=date(int(check_issue_date2[0:4]),int(check_issue_date2[5:7]),int(check_issue_date2[8:10]))
+                local_issue_date=date(int(check_issue_date2[0:4]),int(check_issue_date2[5:7]),int(check_issue_date2[8:10]))
             except ValueError:
                 print("не корректный формат даты / не существующая дата")
                 check_issue_date2 = input("введите дату дэдлайна в формате ДД-ММ-ГГГГ")
-    return issue_date
+    return local_issue_date
 # блок для определения написания день/дня/дней
 issue_date=check_date()
 time_left=issue_date - current_date
@@ -28,18 +28,18 @@ print(string_time_left)
 def dni_dney():
     symbols=len(string_time_left)
     print(symbols)
-    if string_time_left[symbols-1]=='1'and string_time_left[symbols-2]=="1":
-        dni = "дней"
-    elif (string_time_left[symbols-1]=='2' or string_time_left[symbols-1]=="3" or string_time_left[symbols-1]=="4"
+    if string_time_left[symbols-1] == '1'and string_time_left[symbols-2] == "1":
+        local_dni = "дней"
+    elif (string_time_left[symbols-1] == '2' or string_time_left[symbols-1] == "3" or string_time_left[symbols-1] == "4"
           and string_time_left[symbols-2]!="1"):
-        dni="дня"
-    elif string_time_left[symbols-1]=="1" :
-        dni="день"
-    elif (string_time_left[symbols-1]=="2" or string_time_left[symbols-1]=="3" or string_time_left[symbols-1]=="4"):
-        dni="дня"
+        local_dni="дня"
+    elif string_time_left[symbols-1] == "1" :
+        local_dni="день"
+    elif string_time_left[symbols-1] == "2" or string_time_left[symbols-1] == "3" or string_time_left[symbols-1] == "4":
+        local_dni="дня"
     else:
-        dni="дней"
-    return dni
+        local_dni="дней"
+    return local_dni
 dni=dni_dney()
 # вывод дэдлайн истек/до дэдлайна осталось
 if int(string_time_left) > 0:
